3f917d7a9c3a98d4a8aa17ffc866a3d9bef535ef
scarf <greenscarf005@gmail.com>
2026-01-07 09:34:13 +0900

    fix(UI): disable zone overlay by default (#7813)
    fixes https://github.com/cataclysmbn/Cataclysm-BN/issues/7812

--- src/game.h
+++ src/game.h
@@ -982,7 +982,7 @@ class game
 
         bool debug_pathfinding = false; // show NPC pathfinding on overmap ui
         bool debug_submap_grid_overlay = false;
-        bool show_zone_overlay = true;
+        bool show_zone_overlay = false;
 
         /* tile overlays */
         // Toggle all other overlays off and flip the given overlay on/off.
