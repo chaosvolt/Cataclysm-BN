0f0903d2043dc98a689e9c201c120313c9b0982e
Jarreth Weiler <94997385+AzmodiusX@users.noreply.github.com>
2026-02-01 19:34:46 -0800

    fix: fix switching tileset not working due to vehicle fallback (#8019)
    * Vehicle Fallback Fix
    
    * style(autofix.ci): automated formatting
    
    * Update src/cata_tiles.cpp
    
    MONADS
    
    Co-authored-by: scarf <greenscarf005@gmail.com>
    
    * style(autofix.ci): automated formatting
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: scarf <greenscarf005@gmail.com>

--- src/cata_tiles.cpp
+++ src/cata_tiles.cpp
@@ -2696,9 +2696,11 @@ auto cata_tiles::find_tile_looks_like( const std::string &id, TILE_CATEGORY cate
 
         case C_VEHICLE_PART: {
             // vehicle parts start with vp_ for their tiles, but not their IDs
-            const vpart_id base_vpid( id.substr( 3 ) );
-            if( !base_vpid.is_valid() ) {
-                return find_tile_looks_like( id.substr( 3 ), category, looks_like_jumps_limit - 1 );
+            auto base_id = id.substr( 3 );
+            const vpart_id base_vpid( base_id );
+            if( !base_vpid.is_valid() ) {  // Fixed Fallback
+                find_tile_looks_like( base_id, C_FURNITURE, looks_like_jumps_limit - 1 )
+                .or_else( [ &, this] { return find_tile_looks_like( base_id, C_TERRAIN, looks_like_jumps_limit - 1 ); } );
             }
             return find_tile_looks_like( "vp_" + base_vpid.obj().looks_like, category,
                                          looks_like_jumps_limit - 1 );
