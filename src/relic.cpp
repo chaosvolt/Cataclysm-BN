f0a3beebcbc3c1cde2ab01d2c7135cfe555d9401
boniondev <55188452+boniondev@users.noreply.github.com>
2026-03-24 20:20:02 +0100

    fix: Plutonium batteries now charge when outside the reality bubble, plutonium batteries now recharge while stashed (#8342)
    * Add last_relic_process
    
    Just like if you find rotten food because you found it after a year, you will find fully charged relics because you found them after a long time
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    * style(autofix.ci): automated formatting
    
    * Add var to constructor and operator
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    * getter needed because it's private
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    * Add function to update the last_relic_process to the current turn
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    * Multiply the rate by however many charge "cycles" we missed
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    * style(autofix.ci): automated formatting
    
    * Gee I do wonder why the building doesn't work
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    * style(autofix.ci): automated formatting
    
    * This should work
    
    Signed-off-by: boniondev-arch <55188452+boniondev@users.noreply.github.com>
    
    * style(autofix.ci): automated formatting
    
    * Removed redundant variable
    
    Signed-off-by: boniondev-arch <55188452+boniondev@users.noreply.github.com>
    
    * Use inline functions instead
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    * style(autofix.ci): automated formatting
    
    * Forgot to remove this
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    * Possibly fixed the magic full charge
    
    Multiplying rate by ticks would result in exponential charge because you later multiply by rate again, why the hell did I do this?
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    * fix for fringe cases
    
    If the first charge cycle of a plutonium battery occurs inside a player
    inventory, it will not fully charge.
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: boniondev <55188452+boniondev@users.noreply.github.com>
    Signed-off-by: boniondev-arch <55188452+boniondev@users.noreply.github.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/relic.cpp
+++ src/relic.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 
+#include "calendar.h"
 #include "cata_unreachable.h"
 #include "creature.h"
 #include "character.h"
@@ -459,17 +460,34 @@ bool process_recharge_entry( item &itm, const relic_recharge &rech, Character *c
             std::abort();
         }
     }
+    int rate_multiplier = 1; // Not quite sure where to put this
+    int ticks;
+    if( rech.type == relic_recharge_type::time ) {
+        int last_relic_process = itm.get_var( "last_relic_process", 0 );
+        if( last_relic_process == 0 &&
+            carrier ) { // We do not want batteries to fully charge while in a player's inventory
+            ticks = 1;
+        } else {
+            time_duration elapsed = calendar::turn - time_point::from_turn( last_relic_process );
+            ticks = elapsed / rech.interval;
+        }
+        if( ticks > 0 ) {
+            rate_multiplier = ticks;
+        }
+        itm.set_var( "last_relic_process", to_turn<int>( calendar::turn ) );
+    }
     // If it already has ammo, increment charges of ammo inside.
     if( itm.ammo_data() ) {
-        int ammo_charge = clamp( itm.ammo_remaining() + rech.rate, 0, itm.ammo_capacity() );
+        int ammo_charge = clamp( itm.ammo_remaining() + rech.rate * rate_multiplier, 0,
+                                 itm.ammo_capacity() );
         itm.ammo_set( itm.ammo_current(), ammo_charge );
     } else {
         // If not, either give it default ammo, or increment charges directly.
         if( !itm.ammo_types().empty() ) {
-            itm.ammo_set( itm.ammo_default(), clamp( itm.ammo_remaining() + rech.rate, 0,
+            itm.ammo_set( itm.ammo_default(), clamp( itm.ammo_remaining() + rech.rate * rate_multiplier, 0,
                           itm.ammo_capacity() ) );
         } else {
-            itm.charges = clamp( itm.charges + rech.rate, 0, itm.ammo_capacity() );
+            itm.charges = clamp( itm.charges + rech.rate * rate_multiplier, 0, itm.ammo_capacity() );
         }
     }
     if( rech.message ) {
