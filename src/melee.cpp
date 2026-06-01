677df69a0aa5ff8b9e0ad03bee27f12125d5ef64
dedmemdev <unkown3587@gmail.com>
2026-06-01 17:24:26 +0000

    feat(balance): make cvd machine add armor penetration, add armor penetration to bionic weapons (#9206)
    * Update melee.cpp
    
    * make autofix happy
    
    * style(autofix.ci): automated formatting
    
    * Update src/melee.cpp
    
    Co-authored-by: Chaosvolt <chaosvolt@users.noreply.github.com>
    
    * Update src/melee.cpp
    
    Co-authored-by: Chaosvolt <chaosvolt@users.noreply.github.com>
    
    * Update melee.cpp
    
    * Do the thing!
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Chaosvolt <chaosvolt@users.noreply.github.com>

--- src/melee.cpp
+++ src/melee.cpp
@@ -1166,6 +1166,9 @@ void melee::roll_cut_damage( const Character &c, bool crit, damage_instance &di,
     }
 
     int arpen = attack.damage.get_armor_pen( DT_CUT );
+    if( weap.has_flag( flag_DIAMOND ) ) {
+        arpen += cut_dam * 0.35 + 10;
+    }
     float armor_mult = attack.damage.get_armor_mult( DT_CUT );
 
     // 80%, 88%, 96%, 104%, 112%, 116%, 120%, 124%, 128%, 132%
@@ -1247,6 +1250,9 @@ void melee::roll_stab_damage( const Character &c, bool crit, damage_instance &di
     float armor_mult = attack.damage.get_armor_mult( DT_STAB );
     int arpen = attack.damage.get_armor_pen( DT_STAB );
     arpen += c.mabuff_arpen_bonus( DT_STAB );
+    if( weap.has_flag( flag_DIAMOND ) ) {
+        arpen += stab_dam * 0.35 + 10;
+    }
     armor_mult *= c.mabuff_tg_armor_mult( DT_STAB );
 
     if( crit ) {
